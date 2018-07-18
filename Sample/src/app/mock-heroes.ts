f4852c64c7b8486a76f80fbe75b1a73fd75eea18
Pavithra <pavithra15296@outlook.com>
2018-07-18 07:32:56 +0530

    half

---
+++ Sample/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] = [
+    { id: 1, name:'Shinchan' },
+    { id: 2, name:'Jackiechan' },
+    { id: 3, name:'Phineas' },
+    { id: 4, name:'Ferb' },
+    { id: 5, name:'Ninja' },
+    { id: 6, name:'Pokemon' },
+    { id: 7, name:'Heman' },
+    { id: 8, name:'HarryPotter' }
+
+];
